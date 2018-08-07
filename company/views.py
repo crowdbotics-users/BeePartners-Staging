d9b15c16ada7b2f89a1ca8bdc95cb84d199f0354
Sumit Sharma <sumithastir@gmail.com>
2018-08-08 01:19:55 +0530

    Start implementing dashboard page UI

--- company/views.py
+++ company/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class Dashboard(View):
+    template = 'company/index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template, {})
