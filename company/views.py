4649b338ee31e700154d695f1a262d6224581aad
Sumit Sharma <sumithastir@gmail.com>
2018-08-09 23:09:31 +0530

    display the number of company and company names

--- company/views.py
+++ company/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Count
 from django.views import View
-
+from company.models import *
 
 class Dashboard(View):
     template = 'company/index.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template, {})
+        stage_count = CompanyStages.objects.values('STAGE_DESC').annotate(dcount=Count('STAGE_DESC'))
+        print(stage_count)
+        return render(request, self.template, {'stage_count': stage_count})
